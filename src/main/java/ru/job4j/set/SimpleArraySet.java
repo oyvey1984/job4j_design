6aa148926ddc08f03c87b0a813d6f8ef353431dc
oyvey1984 <ayrat.tazetdinov@mail.ru>
2024-09-02 17:23:59 +0300

    1. Реализовать коллекцию Set на массиве [#996]

---
+++ src/main/java/ru/job4j/set/SimpleArraySet.java
@@ -0,0 +1,38 @@
+package ru.job4j.set;
+
+import ru.job4j.collection.SimpleArrayList;
+import java.util.Iterator;
+import java.util.Objects;
+
+public class SimpleArraySet<T> implements SimpleSet<T> {
+
+    private SimpleArrayList<T> set = new SimpleArrayList<>(0);
+
+    @Override
+    public boolean add(T value) {
+        boolean result = false;
+        if (!contains(value)) {
+            set.add(value);
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean contains(T value) {
+        Iterator<T> iterator = set.iterator();
+        boolean result = false;
+        while (iterator.hasNext()) {
+            if (Objects.equals(iterator.next(), value)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+            return set.iterator();
+    }
+}
---
+++ src/test/java/ru/job4j/set/SimpleArraySetTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.set;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class SimpleArraySetTest {
+
+    @Test
+    void whenAddNonNull() {
+        SimpleSet<Integer> set = new SimpleArraySet<>();
+        assertThat(set.add(1)).isTrue();
+        assertThat(set.contains(1)).isTrue();
+        assertThat(set.add(1)).isFalse();
+    }
+
+    @Test
+    void whenAddSomeElementsNonNull() {
+        SimpleSet<Integer> set = new SimpleArraySet<>();
+        assertThat(set.contains(1)).isFalse();
+        assertThat(set.add(1)).isTrue();
+        assertThat(set.contains(1)).isTrue();
+        assertThat(set.add(1)).isFalse();
+        assertThat(set.contains(2)).isFalse();
+        assertThat(set.add(2)).isTrue();
+        assertThat(set.contains(2)).isTrue();
+        assertThat(set.add(2)).isFalse();
+        assertThat(set.contains(3)).isFalse();
+        assertThat(set.add(3)).isTrue();
+        assertThat(set.contains(3)).isTrue();
+        assertThat(set.add(3)).isFalse();
+    }
+
+    @Test
+    void whenAddNull() {
+        SimpleSet<Integer> set = new SimpleArraySet<>();
+        assertThat(set.add(null)).isTrue();
+        assertThat(set.contains(null)).isTrue();
+        assertThat(set.add(null)).isFalse();
+    }
+
+    @Test
+    void whenAddThenIteratorNext() {
+        SimpleSet<Integer> set = new SimpleArraySet<>();
+        assertThat(set.add(null)).isTrue();
+        Iterator<Integer> iterator = set.iterator();
+        assertThat(iterator.next()).isEqualTo(null);
+        assertThatThrownBy(iterator::next)
+                .isInstanceOf(NoSuchElementException.class);
+        assertThat(set.add(1)).isTrue();
+        assertThatThrownBy(iterator::next)
+                .isInstanceOf(ConcurrentModificationException.class);
+    }
+}
