35eb225eb2d5222dfa667175f33d337631b5d876
oyvey1984 <ayrat.tazetdinov@mail.ru>
2024-09-06 20:33:07 +0300

    3. Создать модель User [#999]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,8 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +13,14 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>();
+        Calendar birthday = Calendar.getInstance();
+        User user = new User("Stepan", 2, birthday);
+        User user1 = new User("Stepan", 2, birthday);
+        map.put(user, new Object());
+        map.put(user1, new Object());
     }
 }
