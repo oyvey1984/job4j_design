172c451e246d5455d905d2cd6ad86b25e649ec88
oyvey1984 <ayrat.tazetdinov@mail.ru>
2024-09-21 17:06:11 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,6 +1,7 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -20,12 +21,45 @@ public class SimpleTree<E> implements Tree<E> {
 
     @Override
     public Optional<Node<E>> findBy(E value) {
+        return findByPredicate(eNode -> eNode.value.equals(value));
+//        Optional<Node<E>> result = Optional.empty();
+//        Queue<Node<E>> data = new LinkedList<>();
+//        data.offer(this.root);
+//        while (!data.isEmpty()) {
+//            Node<E> element = data.poll();
+//            if (element.value.equals(value)) {
+//                result = Optional.of(element);
+//                break;
+//            }
+//            data.addAll(element.children);
+//        }
+//        return result;
+    }
+
+    public boolean isBinary() {
+        return findByPredicate(eNode -> eNode.children.size() > 2).isEmpty();
+
+//        boolean result = true;
+//        Queue<Node<E>> data = new LinkedList<>();
+//        data.offer(this.root);
+//        while (!data.isEmpty()) {
+//            Node<E> element = data.poll();
+//            if (element.children.size() > 2) {
+//                result = false;
+//                break;
+//            }
+//            data.addAll(element.children);
+//        }
+//        return result;
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> result = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> element = data.poll();
-            if (element.value.equals(value)) {
+            if (condition.test(element)) {
                 result = Optional.of(element);
                 break;
             }
