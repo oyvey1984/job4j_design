29a3187d71e5bbbac85f3ee61e9dbc0569a15d9f
oyvey1984 <ayrat.tazetdinov@mail.ru>
2024-11-24 23:18:06 +0300

    5.1. BufferedReader [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,40 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    private final String file;
+
+    public LogFilter(String file) {
+        this.file = file;
+    }
+
+    public List<String> filter() {
+        List<String> list = Collections.emptyList();
+        try (BufferedReader input = new BufferedReader(new FileReader("data/log.txt"))) {
+            list = input.lines()
+                    .filter(string -> {
+                boolean rsl = false;
+                String[] str = string.split(" ");
+                if (str[str.length - 2].equals("404")) {
+                    rsl = true;
+                }
+                return rsl;
+            })
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        LogFilter logFilter = new LogFilter("data/log.txt");
+        logFilter.filter().forEach(System.out::println);
+    }
+}
