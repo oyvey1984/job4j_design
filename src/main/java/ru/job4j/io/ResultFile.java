af8c025a38a4949f03ad63f48d7b1e079a9097a6
oyvey1984 <ayrat.tazetdinov@mail.ru>
2024-11-25 23:46:37 +0300

    5.2. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,15 +1,17 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
-            for (int i = 1; i <= 9; i++) {
-                output.write(("1" + " * " + i + " = " + i).getBytes());
-                output.write(System.lineSeparator().getBytes());
-            }
+        try (PrintWriter output = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("data/dataresult.txt")
+                ))) {
+            output.println("Hello, world!");
         } catch (IOException e) {
             e.printStackTrace();
         }
