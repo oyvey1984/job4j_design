116e8d123b45ce00c18fe9e08500e528067d4617
oyvey1984 <ayrat.tazetdinov@mail.ru>
2024-12-08 11:48:08 +0300

    9.0. Файлы и директории. Класс File [#252491]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("C:\\projects");
+        File file = new File("C:\\projects\\job4j_design\\data");
 
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Директория не существует: %s", file.getAbsoluteFile()));
@@ -12,19 +12,14 @@ public class Dir {
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Это не директория: %s", file.getAbsoluteFile()));
         }
-        System.out.printf("Общий объем пространства диска, на котором расположена файловая система: %s%n", file.getTotalSpace());
 
         File[] files = file.listFiles();
             if (files == null) {
                 throw new NullPointerException("Ошибка: метод listFiles() вернул null");
             }
         for (File subfile : files) {
-            if (subfile.isDirectory()) {
-                System.out.printf("Директория: %s%nРазмер директории: %s%n",
-                        subfile.getName(), subfile.length());
-            }
             if (subfile.isFile()) {
-                System.out.printf("Файл: %s%nРазмер файл: %s%n",
+                System.out.printf("Файл: %s%nРазмер файла: %s%n%n",
                         subfile.getName(), subfile.length());
             }
         }
