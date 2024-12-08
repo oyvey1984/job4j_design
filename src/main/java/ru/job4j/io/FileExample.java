da56c779d42e14a5e7495c9daf6bc72eced8c2ee
oyvey1984 <ayrat.tazetdinov@mail.ru>
2024-12-08 11:52:25 +0300

    убрал комменты

--- src/main/java/ru/job4j/io/FileExample.java
+++ src/main/java/ru/job4j/io/FileExample.java
@@ -6,11 +6,10 @@ import java.io.IOException;
 public class FileExample {
     public static void main(String[] args) throws IOException {
         File directory = new File("src/main/java/ru/job4j/io/files");
-        directory.mkdir(); // Создает директорию, если она не существует
+        directory.mkdir();
         File file = new File("src/main/java/ru/job4j/io/files/file.txt");
-        file.createNewFile(); // Создает новый файл, если он не существует
+        file.createNewFile();
 
-        // Вывод свойств файла и директории
         System.out.println("Файл/директория существует?: " + file.exists());
         System.out.println("Это директория?: " + file.isDirectory());
         System.out.println("Это файл?: " + file.isFile());
@@ -24,7 +23,6 @@ public class FileExample {
         System.out.println("Доступен для записи?: " + file.canWrite());
         System.out.println("Длина файла (в байтах): " + file.length());
 
-        // Переименование файла
         File newFile = new File("src/main/java/ru/job4j/io/files/newFile.txt");
         System.out.println("Переименование файла в newFile. Успешно?: " + file.renameTo(newFile));
     }
