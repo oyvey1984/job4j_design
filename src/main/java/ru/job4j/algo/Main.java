f31bd0871d18e1a5564db93fdc92d0fc616db799
Oy vey <oyiveyi@gmail.com>
2025-09-06 17:18:51 +0300

    Aлгоритм двух указателей [#505153] Примеры

---
+++ src/main/java/ru/job4j/algo/Main.java
@@ -0,0 +1,30 @@
+package ru.job4j.algo;
+
+import java.util.Arrays;
+
+public class Main {
+    public static int[] squareSortedArray(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1;
+        int resultIndex = n - 1;
+
+        while (left <= right) {
+            if (Math.abs(arr[left]) > Math.abs(arr[right])) {
+                result[resultIndex] = arr[left] * arr[left];
+                left++;
+            } else {
+                result[resultIndex] = arr[right] * arr[right];
+                right--;
+            }
+            resultIndex--;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] sortedArray = {-4, -2, 0, 2, 3, 5};
+        int[] resultArray = squareSortedArray(sortedArray);
+        System.out.println(Arrays.toString(resultArray));
+    }
+}
