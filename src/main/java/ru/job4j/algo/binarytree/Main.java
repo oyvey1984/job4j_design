32a508c83c6e98170eba3865ddc00024b32717f2
Oy vey <oyiveyi@gmail.com>
2025-10-05 18:10:35 +0300

    Вывод бинарного дерева на консоль [#504978]

---
+++ src/main/java/ru/job4j/algo/binarytree/Main.java
@@ -0,0 +1,8 @@
+package ru.job4j.algo.binarytree;
+
+public class Main {
+    public static void main(String[] args) {
+        ExampleTree exampleTree = new ExampleTree();
+        System.out.println(exampleTree);
+    }
+}
---
+++ src/main/java/ru/job4j/algo/binarytree/VisualNode.java
@@ -0,0 +1,9 @@
+package ru.job4j.algo.binarytree;
+
+public interface VisualNode {
+    VisualNode getLeft();
+
+    VisualNode getRight();
+
+    String getText();
+}
