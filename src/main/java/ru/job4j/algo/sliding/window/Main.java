43cdcb7c676c2650644bc868b1d15ff7a50f04a1
Oy vey <oyiveyi@gmail.com>
2025-10-12 19:08:50 +0300

    Бинарное дерево поиска. Методы добавления, поиска и обхода дерева. [#504983]

--- src/main/java/ru/job4j/algo/sliding/window/Main.java
+++ src/main/java/ru/job4j/algo/sliding/window/Main.java
@@ -1,6 +1,7 @@
 package ru.job4j.algo.sliding.window;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Interval {
     int start;
@@ -17,52 +18,86 @@ class Interval {
     }
 }
 
+class Action {
+    boolean isStart;
+    int time;
+
+    Action(boolean isStart, int time) {
+        this.isStart = isStart;
+        this.time = time;
+    }
+}
+
 public class Main {
 
     public static int[] findMaxOverlapInterval(List<Interval> intervals) {
-        if (intervals.isEmpty()) {
-            return new int[] {-1, -1};
+        if (intervals == null || intervals.isEmpty()) {
+            return new int[] {-1, -1 };
         }
 
-        intervals.sort(Comparator.comparingInt(i -> i.start));
-        PriorityQueue<Interval> activeIntervals = new PriorityQueue<>(Comparator.comparingInt(i -> i.end));
+        List<Action> actions = new ArrayList<>();
+        for (Interval it : intervals) {
+            actions.add(new Action(true, it.start));
+            actions.add(new Action(false, it.end));
+        }
 
+        actions.sort((a, b) -> {
+            if (a.time != b.time) {
+                return Integer.compare(a.time, b.time);
+            }
+            return Boolean.compare(a.isStart, b.isStart);
+        });
+
+        int active = 0;
         int maxOverlap = 0;
         int maxStart = -1;
         int maxEnd = -1;
 
-        for (Interval current : intervals) {
-            while (!activeIntervals.isEmpty() && activeIntervals.peek().end <= current.start) {
-                activeIntervals.poll();
-            }
-
-            activeIntervals.offer(current);
-
-            if (activeIntervals.size() > maxOverlap) {
-                maxOverlap = activeIntervals.size();
-                maxStart = current.start;
-                maxEnd = activeIntervals.peek().end;
-            } else if (activeIntervals.size() == maxOverlap) {
-                int currentEnd = activeIntervals.peek().end;
-                if (currentEnd - current.start < maxEnd - maxStart) {
-                    maxStart = current.start;
-                    maxEnd = currentEnd;
+        for (Action action : actions) {
+            if (action.isStart) {
+                active++;
+                if (active > maxOverlap) {
+                    maxOverlap = active;
+                    maxStart = action.time;
+                    maxEnd = -1;
                 }
+            } else {
+                if (active == maxOverlap && maxOverlap > 0 && maxEnd == -1) {
+                    maxEnd = action.time;
+                }
+                active--;
             }
         }
 
-        return new int[] {maxStart, maxEnd};
+        if (maxStart != -1 && maxEnd == -1) {
+            int minEnd = Integer.MAX_VALUE;
+            for (Interval it : intervals) {
+                if (it.start <= maxStart && it.end < minEnd) {
+                    minEnd = it.end;
+                }
+                if (it.start <= maxStart && it.end < minEnd) {
+                    minEnd = it.end;
+                }
+            }
+            if (minEnd != Integer.MAX_VALUE) {
+                maxEnd = minEnd;
+            } else {
+                maxEnd = intervals.get(0).end;
+            }
+        }
+
+        return new int[] {maxStart, maxEnd };
     }
 
     public static void main(String[] args) {
-        List intervals = new ArrayList<>();
+        List<Interval> intervals = new ArrayList<>();
         intervals.add(new Interval(1, 4));
         intervals.add(new Interval(2, 6));
         intervals.add(new Interval(3, 5));
         intervals.add(new Interval(7, 8));
 
         int[] result = findMaxOverlapInterval(intervals);
-
-        System.out.println("Interval that overlaps the maximum number of intervals: [" + result[0] + ", " + result[1] + "]");
+        System.out.println("Interval that overlaps the maximum number of intervals: ["
+                + result[0] + ", " + result[1] + "]");
     }
 }
